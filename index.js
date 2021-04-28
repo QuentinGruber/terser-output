f711a505c960f5575a4eb40f7eb5293b6f41c776
Quentin Gruber <quentingruber@gmail.com>
2021-04-28 14:18:41 +0200

    fix path issues

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function getAllFiles(dirPath, arrayOfFiles) {
     if (fs.statSync(dirPath + "/" + file).isDirectory()) {
       arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
     } else {
-      arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));
+      arrayOfFiles.push(path.join(process.cwd(), dirPath, "/", file));
     }
   });
 
